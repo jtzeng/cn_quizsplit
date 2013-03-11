ea94ab9d6de9cb26a72513b1df0e2e7ff8ed206d
Justin Zeng <megajustin@hotmail.com>
2013-03-11 23:53:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+cn_quizsplit
+============
+
+Chinese IB BSL Word Splitting Matching Quiz Helper
+
+Essentially generates practice quizzes to prep for word splitting + matching + providing English definitions quizzes. Something that can help save massive amounts of time should the student attempt to manually generate practice quizzes.
+
+	ruby ./cn_quizsplit.rb PATH_TO_DUMP
+
+Example:
+
+	ruby ./cn_quizsplit.rb word_dump.txt
+
+The 'word dump' file is just a plain text file, with each phrase in its own line. By default, the program will split the phrase at the integer floor(len / 2), where len is the length of the phrase (the line). Otherwise, you can manually specify a split point by inserting a comma.
+
+Depending on your terminal, the output from stdout might not format nicely when bluntly copied and pasted over to a word processor; if you have any suggestions on a possible approach or improvements feel free to open a pull request.
+
+Todo
+----
+
+- Output into a nice readable printable file (ODT? PDF? DOC(X)?)
+- Proper space control
+- Possible frontend
+- Provide answers
---
+++ cn_quizsplit.rb
@@ -0,0 +1,75 @@
+# Copyright (C) 2013  Justin Zeng (Whackatre)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# The (optional) delimiter to split a phrase.
+DELIM = ','
+
+# If the terminal output format sucks, modify these values.
+# TODO: Proper space control.
+OFFSET = 65
+TABS = 4
+USCORES = 15
+SPACES = 4
+
+# Hash of phrases.
+words = {}
+
+if ARGV.length < 1
+	puts "Usage: ruby ./cn_quizsplit.rb PATH_TO_DUMP"
+	exit
+else
+	path = ARGV[0]
+	lines = []
+	File.open(path) do |f|
+		lines = f.readlines
+	end
+	# p lines
+	lines.each_with_index do |line, num|
+		num += 1
+
+		# Remove newline.
+		line.strip!
+
+		if line.length == 1
+			raise "Phrase cannot be only 1 character. (line: #{num}"
+		else
+			# The default split index is defined as floor(len / 2).
+			split_idx = (line.length.to_f / 2).floor
+
+			# Don't bother parsing bad input.
+			raise "Only be 1 delimiter per phrase. (line: #{num})" if line.count(DELIM) > 1
+			
+			# If the delimiter exists, split it at that length.
+			split_idx = line.index(DELIM) if line.include?(DELIM)
+
+			words[line[0...split_idx]] = line[split_idx..-1].gsub(DELIM, '')
+		end
+	end
+end
+
+# p words
+
+firsts = words.keys
+seconds = words.values
+
+# Shuffle both the lists of the first words ans second words.
+firsts.shuffle!
+seconds.shuffle!
+
+# Print the randomized sets to stdout.
+firsts.length.times do |n|
+	puts "#{(OFFSET + n).chr}) #{firsts[n]}#{"\t" * TABS} \
+		(#{' ' * SPACES}) #{seconds[n]}\t#{'_' * USCORES}"
+end
